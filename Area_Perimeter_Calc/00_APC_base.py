2e8307f905fbbc267c9dcd682f58d66c56dd35ee
lisap1 <124928251+lisap1@users.noreply.github.com>
2023-05-02 11:03:03 +1200

    Initial Commenting on base

---
+++ Area_Perimeter_Calc/00_APC_base.py
@@ -0,0 +1,52 @@
+# imports
+
+# functions go here
+
+# function for selecting shape (keep dictionary on the outside keep the function generic)
+
+
+# function for checking numbers (num_check)
+
+
+# function for string check (checking if person entered area or perimeter)
+
+    # If triangle or parallelogram ask if they want area or perimeter or both
+    # If triangle: input(do you want area / perimeter)string check
+
+# function for checking how many inputs are needed
+# Circle(1): radius(r)
+# Square(2): side 1(x), side 2(y)
+# Triangle(4): base(x), height(h), side 2(y), side 3(z)
+# Parallelogram(3): base(x), length(y), height(h)
+
+# Maybe do lists for each shape with each required input
+# For item in list
+# input(list[0])
+
+# function for inputting dimensions
+
+# function for doing area and perimeter  calculations
+# (store the formulas in a dictionary somewhere else make this function generic and reusable)
+    # Area = calculator(x, y, h)
+    # Perimeter = calculator(x, y, z)
+
+# setting up dictionaries and lists
+    # Different shape names dictionary
+    # List of formulas for each shape
+# Keep lists for dimensions, (side 1=x, height=h, side 2=y, side 3=z, radius=r)
+
+
+# Start loop
+# Instructions
+
+# Pick what shape
+
+# Area or perimeter( if applicable)
+
+# Enter dimensions
+
+# Calculations
+
+# Add to list
+
+# Display history
---
+++ Area_Perimeter_Calc/01_shape_select_v1.py
@@ -0,0 +1,10 @@
+# set up lists
+valid_shapes = [
+    ["rectangle", "rec", "rec", "r"],
+    ["triangle", "tri", "t"],
+    ["parallelogram", "para", "parallel", "p"],
+    []
+]
+# functions
+
+# Loop until valid answer
