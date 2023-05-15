37d5ec15d69ce0471bfd0dac7d695f9a995d87a1
lisap1 <124928251+lisap1@users.noreply.github.com>
2023-05-15 14:14:10 +1200

    num_check

--- Area_Perimeter_Calc/02_num_check_dimension_prompt_v4.py
+++ Area_Perimeter_Calc/02_num_check_dimension_prompt_v4.py
@@ -1,11 +1,14 @@
 def num_check(num):
     # check if num is within range, 0 - 999
-    if num.isdigit():
-        if 0 < int(num) < 999:
+    print(num)
+    try:
+        if 0 < float(num) < 1000:
             # num.round(num, 4)
+            num = round(float(num), 3)
             print(num)
             return "valid"
-    return "Invalid"
+    finally:
+        return "Invalid"
     # round num to 4 digits
     # allow floats and integers
 
